9de8aff8a880a1b98789da0b8692dd1d3c22d97e
Syer10 <syer10@users.noreply.github.com>
2021-07-23 19:59:06 -0400

    Delete Anime

--- server/src/main/kotlin/eu/kanade/tachiyomi/animesource/ConfigurableAnimeSource.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.animesource
-
-import android.support.v7.preference.PreferenceScreen
-
-interface ConfigurableAnimeSource : AnimeSource {
-
-    fun setupPreferenceScreen(screen: PreferenceScreen)
-}
