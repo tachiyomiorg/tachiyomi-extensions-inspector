80fd63a2c17b2376fc0c2c98b7d3861d8b1f8a7e
arkon <eugcheung94@gmail.com>
2021-11-28 14:04:00 -0500

    Update kotlinx-serialization to match extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ configure(projects) {
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 
-        val kotlinSerializationVersion = "1.1.0"
+        val kotlinSerializationVersion = "1.2.0"
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$kotlinSerializationVersion")
 
