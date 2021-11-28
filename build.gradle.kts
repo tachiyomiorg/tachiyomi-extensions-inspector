eb1d70936761c48d0667f05b02b9f3ffb872d54f
arkon <eugcheung94@gmail.com>
2021-11-28 14:04:07 -0500

    Target JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,13 @@ configure(projects) {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
+            jvmTarget = JavaVersion.VERSION_11.toString()
         }
     }
 
