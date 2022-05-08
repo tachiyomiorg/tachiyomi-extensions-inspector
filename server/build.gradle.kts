3c3f173975949a293f84fe34fc3351c00dadb3a5
arkon <eugcheung94@gmail.com>
2022-05-07 23:54:06 -0400

    Release v0.7.0

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -59,7 +59,7 @@ sourceSets {
 }
 
 // should be bumped with each stable release
-val inspectorVersion = "v0.6.0"
+val inspectorVersion = "v0.7.0"
 
 // counts commit count on master
 val inspectorRevision = runCatching {
